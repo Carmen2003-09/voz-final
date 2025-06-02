d6d6c456c563f67f46946f0b808a89bc53eaeac2
Carmen2003-09 <covadongatrejo904@gmail.com>
2025-06-02 10:26:47 -0600

    pruebafinal

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Prefijo que se usará como palabra clave para activar comandos
 const ordenPrefijo = "NEXA";
 // DEFINICIÓN DEL ENDPOINT PARA LA COMUNICACIÓN CON LA API DE PHP
-const API_URL = "http://3.227.9.55/api-gpt-php/endpoints/chat.php";
+const API_URL = "http://52.3.227.241/api-gpt-php/endpoints/chat.php";
 
 // Espera a que el contenido del DOM esté completamente cargado antes de ejecutar el script
 document.addEventListener("DOMContentLoaded", () => {
